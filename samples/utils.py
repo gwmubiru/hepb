0ed28581bd4d9929c9c8bc8bbc4566b3addaa7ac
Kitutu <pkitutu@gmail.com>
2017-12-11 06:45:26 +0300

    new way of generating sample ids

--- samples/utils.py
+++ samples/utils.py
@@ -27,19 +27,21 @@ def initial_env_number():
 	return "%s%s-" %(utils.year('yy'), utils.month('mm'))
 
 def create_sample_id():
-	smpl_id = 0
+	# smpl_id = 0
 
-	try:
-		samples = Sample.objects.values("vl_sample_id").order_by("-created_at")[:1]
-		vl_sample_id = samples[0].get('vl_sample_id')
-		arr = vl_sample_id.split('/')
-		smpl_id = int(arr[0])
-	except:
-		pass
+	# try:
+	# 	samples = Sample.objects.values("vl_sample_id").order_by("-created_at")[:1]
+	# 	vl_sample_id = samples[0].get('vl_sample_id')
+	# 	arr = vl_sample_id.split('/')
+	# 	smpl_id = int(arr[0])
+	# except:
+	# 	pass
 	
-	smpl_id = str(smpl_id+1)
-	smpl_id = smpl_id.zfill(6)
-	return "%s/%s%s" %(smpl_id, utils.year('yy'), utils.month('mm'))
+	# smpl_id = str(smpl_id+1)
+	# smpl_id = smpl_id.zfill(6)
+	s_count = Sample.objects.filter(created_at__year=utils.year(),created_at__month=utils.month()).count()
+	s_count += 1
+	return "%s/%s%s" %(str(s_count).zfill(6), utils.year('yy'), utils.month('mm'))
 
 def get_facility_by_form(form_number):
 	facility_id = 0
